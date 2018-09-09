347fd2e9be65d54e101b14e8c0cd80231f280dfd
SoyoungPark <3457soso@naver.com>
2018-09-09 18:05:55 +0900

    spring boot 추가

---
+++ Back_Spring/SpringBoot/JPA-board/src/main/java/soyoungpark/me/JpaBoardApplication.java
@@ -0,0 +1,12 @@
+package soyoungpark.me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaBoardApplication.class, args);
+	}
+}
