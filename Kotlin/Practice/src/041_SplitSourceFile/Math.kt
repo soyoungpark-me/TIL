5939e8ef1a4d8333294d2d3404de1a03794e4ac8
3457soso <soyoungpark.me@gmail.com>
2019-06-10 22:35:12 +0900

    Kotlin 예제 41 추가

---
+++ Kotlin/Practice/src/041_SplitSourceFile/Math.kt
@@ -0,0 +1,19 @@
+package `041_SplitSourceFile`
+
+fun max(a: Int, b: Int): Int =
+    if (a > b)
+        a
+    else
+        b
+
+fun min(a: Int, b: Int): Int =
+    if (a < b)
+        a
+    else
+        b
+
+fun abs(num: Int): Int =
+    if (num >= 0)
+        num
+    else
+        -num
